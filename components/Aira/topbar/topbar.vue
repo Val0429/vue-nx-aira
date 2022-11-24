c7239eda8d0be58e6a879f43d6d3dedf404199eb
Val.Liu <val.liu@isapsolution.com>
2022-11-24 15:50:04 +0800

    going rwd menu

--- components/Aira/topbar/topbar.vue
+++ components/Aira/topbar/topbar.vue
@@ -1,10 +1,26 @@
 <template>
-    <div class="header">
+  <!-- <b-navbar class="header justify-content-center" toggleable="lg">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <div class="logo" />
-        <router-link to="/dashboard">Dashboard</router-link>
-        <router-link to="/investigation">Investigation</router-link>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="justify-content-center">
+                <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+                <b-nav-item to="/investigation">Investigation</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+
         <ivc-datetitle class="datetitle" />
-    </div>
+  </b-navbar> -->
+    <b-nav class="header" align="center">
+        <div class="logo" />
+
+        <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+        <b-nav-item to="/investigation">Investigation</b-nav-item>
+        <b-nav-item to="/case">Case</b-nav-item>
+
+        <ivc-datetitle class="datetitle" />
+    </b-nav>
 </template>
 
 <script lang="ts" src="./topbar.vue.ts" />
@@ -35,20 +51,23 @@
         transform: translateY(-50%);
     }
 
-    > a {
+    li.nav-item {
+        padding: 0 45px;
+    }
+    a.nav-link, li.nav-item > a {
         display: inline-block;
         height: 100%;
         font-size: 22px;
         line-height: $top-height;
-        padding: 0 45px;
-        color: #B4BFC0;
+        padding: 0 5px;
+        color: #B4BFC0 !important;
 
         &:hover {
             text-decoration: none;
         }
         &.active {
             font-weight: 700;
-            color: #43A0D1;
+            color: #43A0D1 !important;
         }
     }
 }
