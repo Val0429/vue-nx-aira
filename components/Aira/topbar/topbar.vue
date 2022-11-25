0b418f390e3cde158f91c2b86e67fd5f5fff58c3
Val.Liu <val.liu@isapsolution.com>
2022-11-25 09:13:28 +0800

    dashboard RWD ready

--- components/Aira/topbar/topbar.vue
+++ components/Aira/topbar/topbar.vue
@@ -1,25 +1,22 @@
 <template>
-  <!-- <b-navbar class="header justify-content-center" toggleable="lg">
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <div class="logo" />
-
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="justify-content-center">
-                <b-nav-item to="/dashboard">Dashboard</b-nav-item>
-                <b-nav-item to="/investigation">Investigation</b-nav-item>
-            </b-navbar-nav>
-        </b-collapse>
-
-        <ivc-datetitle class="datetitle" />
-  </b-navbar> -->
     <div class="header justify-content-center">
         <div class="logoarea d-flex flex-row align-items-center">
-            <!-- <iv-button class="b-icon-show-menu" icon="v-icon-show-menu" variant="dark" @click="filterEnabled=!filterEnabled" /> -->
-            <button class="navbar-toggler flex-fill mr-2 d-block d-lg-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button v-b-toggle.sidebar-1 class="navbar-toggler flex-fill mr-2 d-block d-lg-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon v-icon-show-menu"></span>
             </button>
             <div class="logo" />
         </div>
+        <b-sidebar id="sidebar-1" bg-variant="sidebar" no-header shadow>
+            <template #default="{ hide }">
+                <button class="navbar-toggler" type="button" @click="hide()">
+                    <span class="navbar-toggler-icon v-icon-close"></span>
+                </button>
+                <div class="mx-4 mb-2" style="border-top: 1px solid #8A9192"></div>
+                <router-link to="/dashboard">Dashboard</router-link>
+                <router-link to="/investigation">Investigation</router-link>
+                <router-link to="/case">Case</router-link>
+            </template>
+        </b-sidebar>
 
         <div class="navs d-none d-lg-block">
             <router-link to="/dashboard">Dashboard</router-link>
@@ -33,11 +30,16 @@
 
 <script lang="ts" src="./topbar.vue.ts" />
 
+<style lang="scss">
+.bg-sidebar {
+    background: #111111DD;
+}
+</style>
+
 <style lang="scss" scoped>
 .header {
     position: relative;
     height: $top-height;
-    text-align: center;
     background: #00000033;
     mix-blend-mode: normal;
     box-sizing: border-box;
@@ -67,12 +69,14 @@
         transform: translateY(-50%);
     }
 
-    .navs > a {
+    .navs {
+        text-align: center;
+    }
+    .navs > a, .b-sidebar-body > a {
         display: inline-block;
-        height: 100%;
         font-size: 22px;
         line-height: $top-height;
-        padding: 0 45px;
+        padding: 0px 45px;
         color: #B4BFC0 !important;
 
         &:hover {
@@ -84,25 +88,17 @@
         }
     }
 
-    // li.nav-item {
-    //     padding: 0 45px;
-    // }
-    // a.nav-link, li.nav-item > a {
-    //     display: inline-block;
-    //     height: 100%;
-    //     font-size: 22px;
-    //     line-height: $top-height;
-    //     padding: 0 5px;
-    //     color: #B4BFC0 !important;
-
-    //     &:hover {
-    //         text-decoration: none;
-    //     }
-    //     &.active {
-    //         font-weight: 700;
-    //         color: #43A0D1 !important;
-    //     }
-    // }
+    /// Clear Button
+    .v-icon-close {
+        width: 12px;
+        height: 12px;
+        background-image: url("~@/assets/images/close.svg") !important;
+    }
+    button:has(> .v-icon-close) {
+        margin-top: 18px;
+        margin-left: 26px;
+        margin-bottom: 21px;
+    }
 }
 </style>
 
